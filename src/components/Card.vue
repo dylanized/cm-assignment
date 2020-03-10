e255fbb3d7620c75b6169d2a5cdbfa3d15f74970
dylanhassinger <dylan@writeraccess.com>
2020-03-10 14:47:45 -0500

    first draft Card component

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,8 +1,8 @@
 <template>
   <section class="card">
-    <div class="card-header">
-      <img class="card-header-img" src="" alt="" />
-      <slot name="card-header"></slot>
+    <div v-if="img" class="card-header" :style="computedHeaderStyle">
+      <div class="card-header-overlay"></div>
+      <span v-if="badge" class="card-header-badge">{{ badge }}</span>
     </div>
     <div class="card-body">
       <h4 class="card-title">{{ title }}</h4>
@@ -18,43 +18,83 @@ export default {
       type: String,
       default: null,
     },
+    badge: {
+      type: String,
+      default: null,
+    },
+    img: {
+      type: String,
+      default: null,
+    },
+    height: {
+      type: String,
+      default: "200px",
+    }
+  },
+  computed: {
+    computedHeaderStyle() {
+      return "background-image: url('" + require(`@/assets/${this.img}`) + "'); height: " + this.height + ";";
+    },
   },
 };
 </script>
 
 <style>
 .card {
-  width: 340px;
   overflow: hidden;
   border-radius: 1rem;
-  box-shadow: 1px 1px #888888;
+  box-shadow: 0px 13px 35px rgba(0, 30, 47, 0.1);
+  text-align: left;
+  background-color: #fff;
+  line-height: 1.3;
+  font-size: 12px;
+  color: #393C40;
+}
+
+.card.has-action {
+  cursor: pointer;
+}
+
+.card .card-header-overlay {
+  transition: opacity .3s;
+}
+
+.card.has-action:hover .card-header-overlay {
+  opacity: 0;
 }
 
 .card-header {
   height: 200px;
-  background: linear-gradient(180deg, rgba(0, 0, 0, 0.0001) 0%, rgba(22, 27, 35, 0.2) 70.94%, rgba(26, 29, 34, 0.79) 106.25%);
+  background-image: url(../assets/pizza.jpg);
+  background-size: cover;
+  position: relative;
 }
 
-.card-header-img {
-  width: 100%;
-  display: block;
+.card-header-overlay {
+  background: linear-gradient(180deg, rgba(0, 0, 0, 0.0001) 0%, rgba(22, 27, 35, 0.2) 70.94%, rgba(26, 29, 34, 0.79) 106.25%);
+  height: 100%;
 }
 
 .card-header-badge {
-
+  background-color: rgb(255, 255, 255, 0.5);
+  color: #fff;
+  font-weight: bold;
+  padding: .2rem .75rem;
+  border-radius: .75rem;
+  position: absolute;
+  bottom: .5rem;
+  left: .5rem;
 }
 
-.card-header-heart {
-
+.card-title {
+  font-size: 14px;
+  font-weight: 700;
+  margin: 0 0 .6rem 0;
+  line-height: 1.25;
+  color: #0C0C0A;
 }
 
 .card-body {
-  font-size: 1rem;
-  padding: .5rem 1rem;
-  line-height: 1.3;
-}
-
-.card-body .card-title {
-  font-weight: bold;
+  padding: 1rem 1.25rem .35rem 1.25rem;
 }
 </style>
